f11b263a7b23e3a93e2950a541b1766af8dd957f
kzamb <kzamb@kablowka.net>
2017-08-19 10:56:06 +0200

    zmiana

--- src/pierwszydzen/FirstProgram.java
+++ src/pierwszydzen/FirstProgram.java
@@ -7,6 +7,8 @@ public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Raz jeszcze");
+        System.out.println("");
+
 
     }
 }
