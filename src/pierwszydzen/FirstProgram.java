1e8cc70f56b030556e9689be11d8ad858aa6860a
kzamb <kzamb@kablowka.net>
2017-08-19 10:52:46 +0200

    init

---
+++ src/pierwszydzen/FirstProgram.java
@@ -0,0 +1,12 @@
+package pierwszydzen;
+
+/**
+ * Created by vjdwmx on 8/19/2017
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Raz jeszcze");
+
+    }
+}
