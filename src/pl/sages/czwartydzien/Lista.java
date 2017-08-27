8c367ceaebf68bf4de0ad7ad91e97bfa51ffed13
kzamb <kzamb@kablowka.net>
2017-08-27 13:55:31 +0200

    dzien czwarty

--- src/pl/sages/czwartydzien/Lista.java
+++ src/pl/sages/czwartydzien/Lista.java
@@ -6,15 +6,20 @@ public class Lista {
     public static void main(String[] args) {
 
 
-        List<Integer> list = new ArrayList<>();
-
-        Set<String> mapa = new HashSet<>();
-
+        List<Integer> list      = new ArrayList<>();
+        Set<String> a       = new HashSet<>();
+        Set<String> b        = new HashSet<>();
+        Set<String> c        = new HashSet<>();
+        Map<String,Integer> map = new HashMap<>();
 
         list.add(4);
         list.add(1);
 
         System.out.println(list);
+
+
+
+
     }
 }
 
